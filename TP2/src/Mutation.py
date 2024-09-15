335c59dce057d0e868205164a4622259ee10cde3
FSendot <fsendot@itba.edu.ar>
2024-09-15 00:23:29 -0300

    Add remaining methods to Mutation class

--- TP2/src/Mutation.py
+++ TP2/src/Mutation.py
@@ -1,13 +1,11 @@
-
 import random as rnd
 import math
+from enum import Enum
+from typing import Decimal
 from dataclasses import dataclass
-from decimal import Decimal
-from enum import Enum, auto
 from .Player import Player
 from .EVE import EVE
 from .PlayerAttributes import PlayerAttributes
-from .Player import Player
 from .InvalidGenProbabilityValue import InvalidGenProbabilityValue
 
 min_height = Decimal('1.3')
@@ -16,34 +14,55 @@ max_height = Decimal('2.0')
 
 class MutationMethod(Enum):
     SINGLE = "single"
-    LIMITED_MULTI = "limited_multi"
-    UNIFORM_MULTI = "uniform_multi"
-    COMPLETE = "complete"
+    MULTI = "multi"
 
 
 class GenMutation(Enum):
-    HEIGHT = auto()
-    STRENGHT = auto()
-    DEXTERITY = auto()
-    INTELLIGENCE = auto()
-    ENDURANCE = auto()
-    PHYSIQUE = auto()
+    HEIGHT = "height"
+    STRENGHT = "strength"
+    DEXTERITY = "dexterity"
+    INTELLIGENCE = "intelligence"
+    ENDURANCE = "endurance"
+    PHYSIQUE = "physique"
 
 
 @dataclass
 class Configuration:
     mutation: MutationMethod
     pm: Decimal
+    is_uniform: bool
     generational_increment: Decimal = Decimal(0)  # For not-uniform mutation method
     max_genes: int = 1  # For limited_multi mutation method
 
 
 class Mutation:
-    def __init__(self, configuration: Configuration):
-        self.__configuration = configuration
+    def __init__(self, configuration: Configuration, max_points: int):
+        self.config = configuration
+        self.max_points = max_points
+        self.pm = configuration.pm
 
     def mutate(self, generation: int, population: list[Player]):
-        pass
+        return list(map(lambda pl: self.perform(pl),
+                        population))
+
+    def update(self):
+        if self.config.is_uniform:
+            self.pm += self.config.generational_increment
+
+    def perform(self, player: Player):
+        match self.config.mutation_method:
+            case MutationMethod.SINGLE:
+                return Mutation.gen(player,
+                                    self.config.gen_mut,
+                                    self.pm,
+                                    (self.config.lower_bound,
+                                     self.config.higher_bound))
+            case MutationMethod.MULTI:
+                return Mutation.multigen(player,
+                                         self.config.max_genes,
+                                         self.pm,
+                                         (self.config.lower_bound,
+                                          self.config.higher_bound))
 
     @staticmethod
     def gen(self, player: Player, gen_mut: GenMutation,
@@ -100,6 +119,62 @@ class Mutation:
                                                attributes=normalized_p_attr)
                                            .performance))
             return player
+
+    @staticmethod
+    def multigen(self, player: Player, max_genes: int,
+                 p: float, interval: (float, float)) -> Player:
+        gen_list = list(GenMutation)
+        if p > 1 or interval[0] > interval[1] or max_genes > len(gen_list):
+            raise InvalidGenProbabilityValue
+        player_list = self.__get_player_attr_list(player)
+        gens_mutated = set()
+        for i in range(max_genes-1):
+            if rnd.random() > p:
+                mutation = rnd.uniform(interval[0], interval[1])
+                mut = gen_list[rnd.randint(0, len(gen_list) - 1)]
+                while mut not in gens_mutated:
+                    mut = gen_list[rnd.randint(0, len(gen_list) - 1)]
+                match mut:
+                    case GenMutation.HEIGHT:
+                        new_height = Decimal(player
+                                             .height*(Decimal('1')+mutation))
+                        player_list[0] = math.max(min_height,
+                                                  math.min(max_height,
+                                                           new_height))
+                    case GenMutation.STRENGHT:
+                        player_list[1] = int(player
+                                             .p_attr.strength*(1+mutation))
+                    case GenMutation.DEXTERITY:
+                        player_list[2] = int(player
+                                             .p_attr
+                                             .dexterity*(1+mutation))
+                    case GenMutation.INTELLIGENCE:
+                        player_list[3] = int(player
+                                             .p_attr
+                                             .intelligence*(1+mutation))
+                    case GenMutation.ENDURANCE:
+                        player_list[4] = int(player
+                                             .p_attr
+                                             .endurance*(1+mutation))
+                    case GenMutation.PHYSIQUE:
+                        player_list[5] = int(player
+                                             .p_attr.physique*(1+mutation))
+                gens_mutated.add(mut)
+
+        normalized_p = self.__normalize_attr(player_list)
+        normalized_p_attr = PlayerAttributes(
+                                strength=normalized_p[1],
+                                dexterity=normalized_p[2],
+                                intelligence=normalized_p[3],
+                                endurance=normalized_p[4],
+                                physique=normalized_p[5])
+        return Player(height=normalized_p[0],
+                      p_attr=normalized_p_attr,
+                      p_class=player.p_class,
+                      fitness=(EVE(height=player.height,
+                                   p_class=player.p_class,
+                                   attributes=normalized_p_attr)
+                               .performance))
 
     '''
     get_player_attr_list: Returns a list containing the attributes of a
