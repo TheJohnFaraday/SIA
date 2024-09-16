fbe1e101470810321fba13046e6751d00dc49ffc
FSendot <fsendot@itba.edu.ar>
2024-09-15 22:58:56 -0300

    Improve code style in Finish.py

--- TP2/src/Finish.py
+++ TP2/src/Finish.py
@@ -33,6 +33,11 @@ class PopulationStructure:
 
 
 class Finish:
+    THRESHOLD = 5
+    STRUCTURE_DELTA = 40
+    FITNESS_DELTA = Decimal('1.5')
+    HEIGHT_MULTI = 20
+
     def __init__(self, configuration: Configuration):
         self.__configuration = configuration
         self.generation = 1
@@ -43,7 +48,7 @@ class Finish:
 
     def done(self, population: list[Player]) -> bool:
         if self.__eval(population):
-            if self.threshold >= 5:
+            if self.threshold >= self.THRESHOLD:
                 return True
         else:
             self.threshold = 0
@@ -56,42 +61,43 @@ class Finish:
     def __eval(self, population: list[Player]):
         ts = int(time()) - self.ts_start
         if ts >= self.configuration.time_limit:
-            self.threshold += 5
+            self.threshold += self.THRESHOLD
             return True
         for method in self.configuration.methods:
             match method:
                 case FinishMethod.MAX_GENERATIONS:
                     if self.generation >= self.configuration.max_generations:
-                        self.threshold += 5
+                        self.threshold += self.THRESHOLD
                         return True
                 case FinishMethod.ACCEPTABLE_FITNESS:
                     if (
                         self.compute_population_content(population)
                         >= self.configuration.acceptable_fitness
                     ):
-                        self.threshold += 5
+                        self.threshold += self.THRESHOLD
                         return True
                 case FinishMethod.STRUCTURE:
                     structure = self.compute_population_structure(population)
                     if (
                         self.compute_structure_delta(structure, self.prior_p_structure)
-                        < 40
+                        < self.STRUCTURE_DELTA
                     ):
                         self.threshold += 1
                         return True
                 case FinishMethod.CONTENT:
                     content = self.compute_population_content(population)
-                    if content - self.prior_p_content < Decimal("1.5"):
+                    if content - self.prior_p_content < self.FITNESS_DELTA:
                         self.threshold += 1
                         return True
         return False
 
     @staticmethod
     def compute_structure_delta(
+        self,
         struct1: PopulationStructure, struct2: PopulationStructure
     ) -> int:
         delta = 0
-        delta += abs(int(struct1.height-struct2.height)*20)
+        delta += abs(int(struct1.height-struct2.height)*self.HEIGHT_MULTI)
         delta += abs(struct1.strength - struct2.strength)
         delta += abs(struct1.endurance - struct2.endurance)
         delta += abs(struct1.intelligence - struct2.intelligence)
