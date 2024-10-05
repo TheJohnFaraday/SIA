e8e10acccb07d942c87aa6638d27fbba3bb64f13
FSendot <fsendot@itba.edu.ar>
2024-10-04 21:33:06 -0300

    Delete redundant main module in SimplePerceptron

--- TP3/src/SimplePerceptron.py
+++ TP3/src/SimplePerceptron.py
@@ -36,26 +36,3 @@ class SimplePerceptron:
             # convergencia:
             if total_error < 0.01:
                 break
-
-
-if __name__ == "__main__":
-    #AND:
-    and_input = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
-    and_expected = np.array([-1, -1, -1, 1])
-    and_perceptron = SimplePerceptron(len(and_input[0]), 0.001)
-    and_perceptron.train(and_input, and_expected, epochs=100)
-
-    for x, y in zip(and_input, and_expected):
-        pred = and_perceptron.predict(x)
-        print(f"Entrada: {x}, Predicción: {pred}, Valor Esperado: {y}")
-
-    #XOR:
-    xor_input = np.array([[-1, 1], [1, -1], [-1, -1], [1, 1]])
-    xor_expected = np.array([1, 1, -1, -1])
-    xor_perceptron = SimplePerceptron(len(xor_input[0]), 0.001)
-    xor_perceptron.train(xor_input, xor_expected, epochs=10000)
-
-    for x, y in zip(xor_input, xor_expected):
-        pred = xor_perceptron.predict(x)
-        print(f"Entrada: {x}, Predicción: {pred}, Valor Esperado: {y}")
-
