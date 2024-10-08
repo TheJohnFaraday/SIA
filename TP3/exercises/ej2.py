Make plots save in fs, fix ej2 plot scaling

TP3/exercises/ej2.py
@@ -7,7 +7,15 @@ from src.errors import MSE
 from src.utils import unnormalize
 
 
-def train_and_evaluate_perceptron(train_input, train_output, test_input, test_output, test_unnom_output, config, activation_function):
+def train_and_evaluate_perceptron(
+    train_input,
+    train_output,
+    test_input,
+    test_output,
+    test_unnom_output,
+    config,
+    activation_function,
+):
     """Entrena y evalúa un perceptrón (lineal o no lineal)"""
     perceptron = LinearPerceptron(
         len(train_input[0]),
@@ -18,40 +26,66 @@ def train_and_evaluate_perceptron(train_input, train_output, test_input, test_ou
     )
     perceptron.train(train_input, train_output, config.epoch)
 
-    test_predictions, test_errors = perceptron.test(test_input, test_output, test_unnom_output)
+    test_predictions, test_errors = perceptron.test(
+        test_input, test_output, test_unnom_output
+    )
 
     return perceptron, test_errors
 
 
-def plot_mse_comparison(folds, mse_linear, mse_nonlinear, title, ylabel):
+def plot_mse_comparison(folds, mse_linear, mse_nonlinear, title, ylabel, config):
     """Genera una gráfica de barras para comparar el MSE entre lineal y no lineal"""
     plt.figure(figsize=(10, 5))
-    plt.bar(folds - 0.15, mse_linear, width=0.3, label='Lineal')
-    plt.bar(folds + 0.15, mse_nonlinear, width=0.3, label='No Lineal')
-    plt.xlabel('Fold')
+    plt.bar(folds - 0.15, mse_linear, width=0.3, label="Lineal")
+    plt.bar(folds + 0.15, mse_nonlinear, width=0.3, label="No Lineal")
+    plt.xlabel("Fold")
     plt.ylabel(ylabel)
     plt.title(title)
     plt.legend()
-    plt.show()
+    plt.savefig(
+        f"plots/ej2_mse_comparison"
+        f"_lr-{config.learning_rate}"
+        f"_activation-{config.linear_non_linear_activation_function}"
+        f".png"
+    )
 
 
-def plot_epochs_comparison(folds, epochs_linear, epochs_nonlinear, title, ylabel):
+def plot_epochs_comparison(
+    folds, epochs_linear, epochs_nonlinear, title, ylabel, config
+):
     """Genera una gráfica de barras para comparar los epochs alcanzados en cada fold por tipo de perceptrón"""
     plt.figure(figsize=(10, 5))
     bar_width = 0.35
 
     # Gráfico para el perceptrón lineal
-    plt.bar(folds - bar_width/2, epochs_linear, width=bar_width, color='skyblue', label='Linear Perceptron')
+    plt.bar(
+        folds - bar_width / 2,
+        epochs_linear,
+        width=bar_width,
+        color="skyblue",
+        label="Linear Perceptron",
+    )
 
     # Gráfico para el perceptrón no lineal
-    plt.bar(folds + bar_width/2, epochs_nonlinear, width=bar_width, color='lightgreen', label='Non-Linear Perceptron')
+    plt.bar(
+        folds + bar_width / 2,
+        epochs_nonlinear,
+        width=bar_width,
+        color="lightgreen",
+        label="Non-Linear Perceptron",
+    )
 
-    plt.xlabel('Fold')
+    plt.xlabel("Fold")
     plt.ylabel(ylabel)
     plt.title(title)
     plt.xticks(folds)
     plt.legend()
-    plt.show()
+    plt.savefig(
+        f"plots/ej2_epoch_comparison"
+        f"_lr-{config.learning_rate}"
+        f"_activation-{config.linear_non_linear_activation_function}"
+        f".png"
+    )
 
 
 def train_with_kfold(inputs, outputs, unnom_outputs, config):
@@ -67,31 +101,71 @@ def train_with_kfold(inputs, outputs, unnom_outputs, config):
 
         train_input, test_input = inputs[train_index], inputs[test_index]
         train_output, test_output = outputs[train_index], outputs[test_index]
-        train_unnom_output, test_unnom_output = unnom_outputs[train_index], unnom_outputs[test_index]
+        train_unnom_output, test_unnom_output = (
+            unnom_outputs[train_index],
+            unnom_outputs[test_index],
+        )
 
         # Entrenamos y evaluamos el perceptrón lineal
         print("Linear Perceptron")
         linear_perceptron, test_lineal_errors = train_and_evaluate_perceptron(
-            train_input, train_output, test_input, test_output, test_unnom_output, config, ActivationFunction.LINEAR
+            train_input,
+            train_output,
+            test_input,
+            test_output,
+            test_unnom_output,
+            config,
+            ActivationFunction.LINEAR,
         )
         mse_linear.append(linear_perceptron.final_error[-1])
-        epochs_linear.append(len(linear_perceptron.final_epochs))  # Guardamos el número de epochs lineales
+        epochs_linear.append(
+            len(linear_perceptron.final_epochs)
+        )  # Guardamos el número de epochs lineales
 
         # Entrenamos y evaluamos el perceptrón no lineal
         print("No Linear Perceptron")
         non_linear_perceptron, test_non_lineal_errors = train_and_evaluate_perceptron(
-            train_input, train_output, test_input, test_output, test_unnom_output, config, config.linear_non_linear_activation_function
+            train_input,
+            train_output,
+            test_input,
+            test_output,
+            test_unnom_output,
+            config,
+            config.linear_non_linear_activation_function,
         )
         mse_nonlinear.append(non_linear_perceptron.final_error[-1])
-        epochs_nonlinear.append(len(non_linear_perceptron.final_epochs))  # Guardamos el número de epochs no lineales
+        epochs_nonlinear.append(
+            len(non_linear_perceptron.final_epochs)
+        )  # Guardamos el número de epochs no lineales
 
         fold += 1
 
     # Gráfica de comparación del MSE para el entrenamiento y la evaluación
     folds = np.arange(1, len(mse_linear) + 1)
-    plot_mse_comparison(folds, mse_linear, mse_nonlinear, 'Comparison of training MSE', 'Training MSE')
-    plot_mse_comparison(folds, test_lineal_errors, test_non_lineal_errors, 'Comparison of testing MSE', 'Testing MSE')
-    plot_epochs_comparison(folds, epochs_linear, epochs_nonlinear, 'Epochs Reached per Fold', 'Epochs')
+    plot_mse_comparison(
+        folds,
+        mse_linear,
+        mse_nonlinear,
+        "Comparison of training MSE",
+        "Training MSE",
+        config,
+    )
+    plot_mse_comparison(
+        folds,
+        test_lineal_errors,
+        test_non_lineal_errors,
+        "Comparison of testing MSE",
+        "Testing MSE",
+        config,
+    )
+    plot_epochs_comparison(
+        folds,
+        epochs_linear,
+        epochs_nonlinear,
+        "Epochs Reached per Fold",
+        "Epochs",
+        config,
+    )
 
 
 def train_without_kfold(inputs, outputs, config):
@@ -124,26 +198,51 @@ def train_without_kfold(inputs, outputs, config):
     non_linear_perceptron.train(train_input, train_output, config.epoch)
 
     # Normalización de los errores de entrenamiento para graficar
-    min_train_error = min(min(linear_perceptron.train_errors), min(non_linear_perceptron.train_errors))
-    max_train_error = max(max(linear_perceptron.train_errors), max(non_linear_perceptron.train_errors))
+    min_train_error = min(
+        min(linear_perceptron.train_errors), min(non_linear_perceptron.train_errors)
+    )
+    max_train_error = max(
+        max(linear_perceptron.train_errors), max(non_linear_perceptron.train_errors)
+    )
 
-    linear_perceptron.train_errors = [(error - min_train_error) / (max_train_error - min_train_error) for error in linear_perceptron.train_errors]
-    non_linear_perceptron.train_errors = [(error - min_train_error) / (max_train_error - min_train_error) for error in non_linear_perceptron.train_errors]
+    linear_perceptron.train_errors = [
+        (error - min_train_error) / (max_train_error - min_train_error)
+        for error in linear_perceptron.train_errors
+    ]
+    non_linear_perceptron.train_errors = [
+        (error - min_train_error) / (max_train_error - min_train_error)
+        for error in non_linear_perceptron.train_errors
+    ]
 
     # Determinar el mínimo de los final_epochs entre ambos perceptrones
-    min_epochs = min(len(linear_perceptron.final_epochs), len(non_linear_perceptron.final_epochs))
+    min_epochs = min(
+        len(linear_perceptron.final_epochs), len(non_linear_perceptron.final_epochs)
+    )
 
     # Gráfica de evolución del error de entrenamiento
     plt.figure(figsize=(10, 5))
-    plt.plot(linear_perceptron.final_epochs[:min_epochs], linear_perceptron.train_errors[:min_epochs], label='Lineal (train_proportion)')
-    plt.plot(non_linear_perceptron.final_epochs[:min_epochs], non_linear_perceptron.train_errors[:min_epochs], label='No Lineal (train_proportion)')
-    plt.xlabel('Epoch')
-    plt.ylabel('Error (MSE)')
-    plt.title('Evolution of MSE during training (without K-Fold)')
+    plt.plot(
+        linear_perceptron.final_epochs[:min_epochs],
+        linear_perceptron.train_errors[:min_epochs],
+        label="Lineal (train_proportion)",
+    )
+    plt.plot(
+        non_linear_perceptron.final_epochs[:min_epochs],
+        non_linear_perceptron.train_errors[:min_epochs],
+        label="No Lineal (train_proportion)",
+    )
+    plt.xlabel("Epoch")
+    plt.ylabel("Error (MSE)")
+    plt.title("Evolution of MSE during training (without K-Fold)")
     plt.legend()
-    plt.xlim(0, min_epochs)
+    plt.xlim(0, min_epochs if min_epochs < 200 else 200)
     plt.ylim(0, 1)  # Normalizado
-    plt.show()
+    plt.savefig(
+        f"plots/ej2_mse_comparison_without_kfold"
+        f"_lr-{config.learning_rate}"
+        f"_activation-{config.linear_non_linear_activation_function}"
+        f".png"
+    )
 
 
 def ej2(config: Configuration):
@@ -156,5 +255,3 @@ def ej2(config: Configuration):
 
     # Entrenamiento sin K-Fold usando train_proportion
     train_without_kfold(inputs, outputs, config)
-
-

TP3/main.py
@@ -33,9 +33,9 @@ def main():
         np.random.seed(config.random_seed)
 
     # ej1(config)
-    # ej2(config)
+    ej2(config)
     # ej3(config)
-    ej4(config)
+    # ej4(config)
 
 
 if __name__ == "__main__":