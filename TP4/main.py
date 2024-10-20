d62d7022313fb448ec66dea3935c904fcb8af3c3
Martín E. Zahnd <mzahnd@itba.edu.ar>
2024-10-20 17:40:37 -0300

    Europe PCA

---
+++ TP4/main.py
@@ -0,0 +1,209 @@
+import csv
+import logging
+import sys
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+
+from src.grapher import Coordinates, scatter_pca, boxplot_pca, pc1_pca
+
+from sklearn.decomposition import PCA
+
+
+@dataclass(frozen=True)
+class EuropeInput:
+    country: list[str]
+    area: list[int]
+    gdp: list[int]
+    inflation: list[float]
+    life_expectancy: list[float]
+    military: list[float]
+    population_growth: list[float]
+    unemployment: list[float]
+
+
+@dataclass(frozen=True)
+class Europe:
+    country: list[str]
+    data: np.array
+
+
+def load_csv(path: str):
+    europe: EuropeInput | None = None
+    with open(path, newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if not europe:
+                europe = EuropeInput(
+                    country=[row["Country"]],
+                    area=[int(row["Area"])],
+                    gdp=[int(row["GDP"])],
+                    inflation=[float(row["Inflation"])],
+                    life_expectancy=[float(row["Life.expect"])],
+                    military=[float(row["Military"])],
+                    population_growth=[float(row["Pop.growth"])],
+                    unemployment=[float(row["Unemployment"])],
+                )
+            else:
+                europe.country.append(row["Country"])
+                europe.area.append(int(row["Area"]))
+                europe.gdp.append(int(row["GDP"]))
+                europe.inflation.append(float(row["Inflation"]))
+                europe.life_expectancy.append(float(row["Life.expect"]))
+                europe.military.append(float(row["Military"]))
+                europe.population_growth.append(float(row["Pop.growth"]))
+                europe.unemployment.append(float(row["Unemployment"]))
+
+    return europe
+
+
+def europe_input_to_dataframe(europe_input: EuropeInput):
+    # return pd.DataFrame(
+    #     np.array(
+    #         [
+    #             europe_input.area,
+    #             europe_input.gdp,
+    #             europe_input.inflation,
+    #             europe_input.life_expectancy,
+    #             europe_input.military,
+    #             europe_input.population_growth,
+    #             europe_input.unemployment,
+    #         ]
+    #     ),
+    #     columns=europe_input.country,
+    # )
+
+    matrix = np.array(
+        [
+            europe_input.area,
+            europe_input.gdp,
+            europe_input.inflation,
+            europe_input.life_expectancy,
+            europe_input.military,
+            europe_input.population_growth,
+            europe_input.unemployment,
+        ]
+    )
+
+    return pd.DataFrame(
+        matrix.transpose(),
+        columns=[
+            "Area",
+            "GDP",
+            "Inflation",
+            "Life Expectancy",
+            "Military",
+            "Population Growth",
+            "Unemployment",
+        ],
+    )
+
+
+def standardize_dataframe(df: pd.DataFrame):
+    return df.apply(lambda x: (x - x.mean()) / x.std(), axis=0)
+
+
+def pca_two(europe: pd.DataFrame, europe_std: pd.DataFrame):
+    pca = PCA(n_components=2)
+    pca_data = pca.fit_transform(europe_std)
+    pca_components = pca.components_
+
+    df_pca_dict = {}
+    for idx, component in enumerate(pca_components):
+        df_pca_dict[f"PC{idx+1}"] = component
+
+    df = pd.DataFrame.from_dict(
+        df_pca_dict,
+    )
+    df["Variable"] = europe_std.columns
+    df = df.set_index("Variable")
+
+    scatter_pca(
+        "Análisis PCA",
+        "pca",
+        "sklearn",
+        pca_data,
+        df,
+        europe_input.country,
+        scale_factor=3,
+        text_scale_factor=3.2,
+        arrow_text_offset=Coordinates(x=0, y=0.1),
+    )
+
+    boxplot_pca(
+        "Variables sin normalizar",
+        "pca",
+        "not_normalized",
+        europe,
+        label_x="Variable",
+        label_y="Valor",
+    )
+    boxplot_pca(
+        "Variables normalizadas",
+        "pca",
+        "normalized",
+        europe_std,
+        label_x="Variable",
+        label_y="Valor",
+    )
+
+
+def pca_one(europe_input: EuropeInput, europe_std: pd.DataFrame):
+    pca = PCA(n_components=1)
+    pca_data = pca.fit_transform(europe_std)
+
+    df_pca_dict = {
+        "PC1": pca_data[:, 0]
+    }
+
+    df_country = pd.DataFrame.from_dict(
+        df_pca_dict,
+    )
+    df_country["Country"] = europe_input.country
+    df_country = df_country.set_index("Country")
+
+    pc1_pca(
+        "Análisis PC1",
+        "pca",
+        "pc1_countries",
+        df_country.index.values,
+        df_country["PC1"].values,
+        "País",
+        "PC1",
+    )
+
+    pca_components = pca.components_
+
+    df_pca_dict = {}
+    for idx, component in enumerate(pca_components):
+        df_pca_dict[f"PC{idx + 1}"] = component
+
+    df_variables = pd.DataFrame.from_dict(
+        df_pca_dict,
+    )
+    df_variables["Variable"] = europe_std.columns
+    df_variables = df_variables.set_index("Variable")
+
+    pc1_pca(
+        "Análisis PC1",
+        "pca",
+        "pc1_vars",
+        df_variables.index.values,
+        df_variables["PC1"].values,
+        "Variables",
+        "PC1",
+    )
+
+
+if __name__ == "__main__":
+    europe_input = load_csv("./dataset/europe.csv")
+    if not europe_input:
+        logging.error("No Input parsed. Is the filepath OK?")
+        sys.exit(1)
+
+    europe = europe_input_to_dataframe(europe_input)
+    europe_std = standardize_dataframe(europe)
+
+    pca_one(europe_input, europe_std)
+    pca_two(europe, europe_std)
---
+++ TP4/src/grapher.py
@@ -0,0 +1,193 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+from sklearn.decomposition import PCA
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Coordinates:
+    x: float | int
+    y: float | int
+
+
+CUSTOM_PALETTE = [
+    "#508fbe",  # blue
+    "#f37120",  # orange
+    "#4baf4e",  # green
+    "#f2cb31",  # yellow
+    "#c178ce",  # purple
+    "#cd4745",  # red
+    "#9ef231",  # light green
+    "#50beaa",  # green + blue
+    "#8050be",  # violet
+    "#cf1f51",  # magenta
+]
+GREY = "#6f6f6f"
+LIGHT_GREY = "#bfbfbf"
+
+PLT_THEME = {
+    "axes.prop_cycle": plt.cycler(color=CUSTOM_PALETTE),  # Set palette
+    "axes.spines.top": False,  # Remove spine (frame)
+    "axes.spines.right": False,
+    "axes.spines.left": True,
+    "axes.spines.bottom": True,
+    "axes.edgecolor": LIGHT_GREY,
+    "axes.titleweight": "normal",  # Optional: ensure title weight is normal (not bold)
+    "axes.titlelocation": "center",  # Center the title by default
+    "axes.titlecolor": GREY,  # Set title color
+    "axes.labelcolor": GREY,  # Set labels color
+    "axes.labelpad": 10,
+    "axes.titlesize": 8,
+    "xtick.bottom": False,  # Remove ticks on the X axis
+    "ytick.labelcolor": GREY,  # Set Y ticks color
+    "ytick.color": GREY,  # Set Y label color
+    "savefig.dpi": 128,
+    "legend.frameon": False,
+    "legend.labelcolor": GREY,
+}
+
+plt.style.use(PLT_THEME)
+sns.set_palette(CUSTOM_PALETTE)
+sns.set_style(PLT_THEME)
+
+
+def scatter_pca(
+    title: str,
+    exercise: str,
+    plot_name: str,
+    data_pca,
+    df: pd.DataFrame,
+    countries: list[str],
+    scale_factor: int | float = 1,
+    text_scale_factor: int | float = 1,
+    arrow_text_offset: Coordinates = Coordinates(0, 0),
+    subtitle: str | None = None,
+):
+    fig = plt.figure(figsize=(12, 10))
+    ax = fig.add_subplot(1, 1, 1)
+
+    xs = data_pca[:, 0]
+    ys = data_pca[:, 1]
+
+    ax.scatter(
+        xs,
+        ys,
+        c=[CUSTOM_PALETTE[i % len(CUSTOM_PALETTE)] for i in range(len(xs))],
+        alpha=0.8,
+    )
+
+    for i in range(len(countries)):
+        ax.text(xs[i], ys[i] + 0.1, countries[i], ha="center", fontsize=8)
+
+    for idx, variable_label in enumerate(df.index.values):
+        head_x = df.iloc[idx, 0]
+        head_y = df.iloc[idx, 1]
+        ax.arrow(
+            0,
+            0,
+            head_x * scale_factor,
+            head_y * scale_factor,
+            color=CUSTOM_PALETTE[1],
+            alpha=0.5,
+            head_width=0.05,
+            head_length=0.09,
+        )
+
+        right_offset = Coordinates(
+            x=arrow_text_offset.x,
+            y=arrow_text_offset.y if head_y >= 0 else -1 * arrow_text_offset.y,
+        )
+
+        ax.text(
+            head_x * text_scale_factor + right_offset.x,
+            head_y * text_scale_factor + right_offset.y,
+            variable_label,
+            color=CUSTOM_PALETTE[1],
+            ha="center",
+            va="center",
+            fontsize=10,
+        )
+
+    ax.set_ylabel(df.columns[1])
+    ax.set_xlabel(df.columns[0])
+
+    fig.suptitle(title)
+    if subtitle:
+        ax.set_title(subtitle)
+
+    plt.savefig(f"plots/{exercise}_{plot_name}_biplot.png")
+
+
+def boxplot_pca(
+    title: str,
+    exercise: str,
+    plot_name: str,
+    df: pd.DataFrame,
+    label_x: str,
+    label_y: str,
+    subtitle: str | None = None,
+):
+    fig = plt.figure(figsize=(12, 10))
+    ax = fig.add_subplot(1, 1, 1)
+
+    ax = sns.boxplot(df, boxprops=dict(alpha=0.65))
+    sns.despine(offset=10, trim=True)
+
+    ax.set_xlabel(label_y)
+    ax.set_xlabel(label_x)
+
+    fig.suptitle(title)
+    if subtitle:
+        ax.set_title(subtitle)
+
+    plt.savefig(f"plots/{exercise}_{plot_name}_boxplot.png")
+
+
+def pc1_pca(
+    title: str,
+    exercise: str,
+    plot_name: str,
+    xs,
+    ys,
+    label_x: str,
+    label_y: str,
+    subtitle: str | None = None,
+):
+
+    fig = plt.figure(figsize=(12, 10))
+    ax = fig.add_subplot(1, 1, 1)
+
+    ax.bar(xs, ys, alpha=0.8, color=CUSTOM_PALETTE)
+
+    x1, x2, y1, y2 = plt.axis()
+    if y1 > -1 and y2 < 1:
+        plt.ylim(-1, 1)
+        y1 = -1
+        y2 = 1
+
+    for i in range(len(xs)):
+        if y1 < -1 and y2 > 1:
+            # Big values in Y axis
+            right_ys = ys[i] + 0.1 if ys[i] >= 0 else ys[i] - 0.18
+        else:
+            right_ys = ys[i] + 0.05 if ys[i] >= 0 else ys[i] - 0.1
+
+        ax.text(xs[i], right_ys, f"{ys[i]:.2f}", ha="center", fontsize=10)
+
+    ax.set_ylabel(label_y)
+    ax.set_xlabel(label_x)
+
+    plt.xticks(rotation=45, ha="right")
+    if len(ys) > 10:
+        plt.tick_params(bottom=True)
+
+    fig.suptitle(title)
+    if subtitle:
+        ax.set_title(subtitle)
+
+    plt.savefig(f"plots/{exercise}_{plot_name}_pc1.png")
